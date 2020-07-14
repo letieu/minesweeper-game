08a7cd5c9fce379f962ac535483c2da4fca8b11d
letieu <letieu8@gmail.com>
2020-07-14 18:20:22 +0700

    Updates

---
+++ precache-manifest.21eaeec9f12e8b9672ab0de1fb659676.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "304b1142247fbce4de00fb2e340741e7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d575b14cdf9ec93fb285",
+    "url": "/static/css/main.507dfff4.chunk.css"
+  },
+  {
+    "revision": "0e92a54747d6d24966a8",
+    "url": "/static/js/2.ce8d0fec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ce8d0fec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d575b14cdf9ec93fb285",
+    "url": "/static/js/main.11716f09.chunk.js"
+  },
+  {
+    "revision": "e836d65d5f5baf4606c9",
+    "url": "/static/js/runtime-main.49e8c79e.js"
+  },
+  {
+    "revision": "0229e74644e183daa6582859fdc3526c",
+    "url": "/static/media/download.0229e746.png"
+  }
+]);
